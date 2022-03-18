8611964924a610c450fc34dc024123a26d9f676e
An-225 <66622436+An-225@users.noreply.github.com>
2022-03-17 21:49:49 -0300

    init backend

---
+++ back/src/main/java/com/ionnote/IonNoteApplication.java
@@ -0,0 +1,13 @@
+package com.ionnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IonNoteApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/com/ionnote/IonNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IonNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
