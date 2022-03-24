0076f3a2cf0e671615744951dcdc6a1cd9838f8b
An-225 <66622436+An-225@users.noreply.github.com>
2022-03-23 21:33:48 -0300

    MongoDB connection

--- back/src/main/java/com/ionnote/IonNoteApplication.java
+++ back/src/main/java/com/ionnote/IonNoteApplication.java
@@ -2,8 +2,10 @@ package com.ionnote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class IonNoteApplication {
 
     public static void main(String[] args) {
