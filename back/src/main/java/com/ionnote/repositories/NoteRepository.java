bff0441cdaa9c687750922170df24ff64594ef6d
An-225 <66622436+An-225@users.noreply.github.com>
2022-05-05 01:47:15 -0300

    Note refactor and user limits

--- back/src/main/java/com/ionnote/repositories/NoteRepository.java
+++ back/src/main/java/com/ionnote/repositories/NoteRepository.java
@@ -3,7 +3,9 @@ package com.ionnote.repositories;
 import com.ionnote.entities.Note;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface NoteRepository extends MongoRepository<Note, UUID> {
+    List<Note> findAllByOwnerId(String id);
 }
