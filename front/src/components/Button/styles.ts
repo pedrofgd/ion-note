b5b634e0b28722bfe03415c6d6d2cd4b049524c5
Pedro Figueiredo <mnopedrodias@gmail.com>
2022-05-05 12:24:50 -0300

    front: alterar estilizacao botoes

--- front/src/components/Button/styles.ts
+++ front/src/components/Button/styles.ts
@@ -1,32 +1,55 @@
 import styled from 'styled-components'
 
 export const Container = styled.button`
-   display: flex;
-   align-items: center;
-   justify-content: center;
-   flex-shrink: 0;
+  align-items: center;
+  background-color: #FFFFFF;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: .25rem;
+  box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
+  box-sizing: border-box;
+  color: rgba(0, 0, 0, 0.85);
+  cursor: pointer;
+  display: inline-flex;
+  font-family: system-ui,-apple-system,system-ui,"Helvetica Neue",Helvetica,Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  justify-content: center;
+  line-height: 1.25;
+  margin: 0 0 26px;
+  min-height: 3rem;
+  padding: calc(.875rem - 1px) calc(1.5rem - 1px);
+  position: relative;
+  text-decoration: none;
+  transition: all 150ms;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: baseline;
+  width: 100%;
 
-   width: 100%;
-   height: 45px;
-   border-radius: 5px;
-   background-color: ${props => props.theme.colors.button};
-   border: none;
-
-   position: relative;
-   cursor: pointer;
-   transition: 0.3s;
-
-   margin-bottom: 40px;
-
-   :hover {
-      background-color: ${props => props.theme.colors.buttonOpacity}; 
-      color: ${props => props.theme.colors.black}
+   :hover,
+   :focus {
+      border-color: rgba(0, 0, 0, 0.15);
+      box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
+      color: rgba(0, 0, 0, 0.65);
    }
 
-   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+   :hover {
+      transform: translateY(-1px);
+   }
+
+   :active {
+      background-color: #F0F0F1;
+      border-color: rgba(0, 0, 0, 0.15);
+      box-shadow: rgba(0, 0, 0, 0.06) 0 2px 4px;
+      color: rgba(0, 0, 0, 0.65);
+      transform: translateY(0);
+   }
 `
 
 export const Text = styled.span`
    font-size: 16px;
    font-weight: bold;
+
+   /* background-color: #fcf9d5; */
 `
